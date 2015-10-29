f9181a5515411626c5eb87b9157200bbcdcc8e7f
Christos Manolis <cmanolis9275@bg-mmcte-17.local>
2015-10-29 09:31:12 -0500

    Initial Commit

---
+++ tipCalculator2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipCalculator2
+//
+//  Created by Christos Manolis on 10/29/15.
+//  Copyright © 2015 Christos Manolis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
